244ba97cf7e2f2db4b2865c2a91b315c362f4737
Jackson <michael.negrete16@gmail.com>
2022-03-09 23:01:05 -0500

    My first and last commit

---
+++ src/data/monedas.js
@@ -0,0 +1,9 @@
+const monedas = [
+    {id: 'USD', nombre: 'Dolar de estados unidos'},
+    {id: 'COP', nombre: 'Pesos Colombianos'},
+    {id: 'EUR', nombre: 'Euro '},
+  ]
+
+  export{
+      monedas
+  }
